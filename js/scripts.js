d0a7d259bc7a8818a7fb6c98a51f353065e90323
ebruh <junada87@gmail.com>
2018-05-13 19:46:35 +0300

    Added functionality for loosing, holding and new game

--- js/scripts.js
+++ js/scripts.js
@@ -1,25 +1,68 @@
-var scores = [0,0];
-var activePlayer = 0;
-var roundScore = 0;
+var scores,
+    activePlayer,
+    roundScore,
+    gamePlaying;
 
+init();
 
-$(document).ready(function() {
-   
+function init() {
+    scores = [0, 0];
+    activePlayer = 0;
+    roundScore = 0;
+    gamePlaying = true;    
+    $('#current-0').text(roundScore);
+    $('#current-1').text(roundScore);
+    $('#player-0').addClass('pulsate');
+    $('#player-1').removeClass('pulsate');
+    $('#score-0').text(scores[activePlayer]);
+    $('#score-1').text(scores[activePlayer]);
+}
+
+function looseRound() {
+    roundScore = 0;
+    $('#current-' + activePlayer).text(roundScore);
+    $('#player-' + activePlayer).toggleClass('pulsate');
+    switchPlayer();
+    $('#player-' + activePlayer).toggleClass('pulsate');
+
+}
+
+function switchPlayer() {
+    if (activePlayer === 0) {
+        activePlayer = 1
+    } else {
+        activePlayer = 0
+    }
+}
+
+$('#btn-roll').click(function () {
+    if (gamePlaying) {
+        var dice = Math.floor(Math.random() * 6) + 1;
+        var diceElement = $('#diceImage');
+        diceElement.attr("src", "../img/die-" + dice + ".svg");
+        if (dice > 1) {
+            roundScore += dice;
+            $('#current-' + activePlayer).text(roundScore);
+        } else {
+            looseRound()
+        }
+    }
 })
-$('#btn-roll').click(function(){
-    var dice = Math.floor(Math.random() * 6) + 1;
-    var diceElement = $('#diceImage');
-   diceElement.attr("src", "../img/die-"+ dice +".svg");
-    roundScore += dice;
-    
-    
-    
+
+$('#hold-btn').click(function () {
+    if (gamePlaying) {
+        scores[activePlayer] += roundScore;
+        $('#score-' + activePlayer).text(scores[activePlayer]);
+
+        if (scores[activePlayer] >= 20) {
+            alert('Player ' + (activePlayer + 1) + ' has won!')
+            gamePlaying = false;
+        } else {
+            looseRound()
+
+        }
+
+    }
 })
-var firstName = "ibrahim";
-var lastName = "Makaraan"
-var fullName = firstName + " hello  "+ lastName;
-console.log(fullName);
 
-
-
-
+$('#btn-new').click(init)
