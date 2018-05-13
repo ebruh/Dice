15b40c314ebcf178d91422a18a64b3bcc1a70b43
ebruh <junada87@gmail.com>
2018-05-13 18:34:24 +0300

    Change image according to random number

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,25 @@
 var scores = [0,0];
 var activePlayer = 0;
+var roundScore = 0;
+
+
+$(document).ready(function() {
+   
+})
+$('#btn-roll').click(function(){
+    var dice = Math.floor(Math.random() * 6) + 1;
+    var diceElement = $('#diceImage');
+   diceElement.attr("src", "../img/die-"+ dice +".svg");
+    roundScore += dice;
+    
+    
+    
+})
+var firstName = "ibrahim";
+var lastName = "Makaraan"
+var fullName = firstName + " hello  "+ lastName;
+console.log(fullName);
+
+
+
 
